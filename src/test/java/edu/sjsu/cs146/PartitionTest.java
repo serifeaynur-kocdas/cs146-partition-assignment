03e99b6b4746ebfbeb121febe4027fa89bc63b6f
Serife Aynur Kocdas <kocdasserifeaynur@gmail.com>
2026-02-09 18:25:32 -0800

    Add unit tests for Hoare partition

--- src/test/java/edu/sjsu/cs146/PartitionTest.java
+++ src/test/java/edu/sjsu/cs146/PartitionTest.java
@@ -34,4 +34,33 @@ public class PartitionTest {
         assertEquals(3, p);
         assertArrayEquals(new int[]{3,1,2,5,9,6,7,84}, a);
     }
+    @Test
+    void hoare_sortedArray() {
+        int[] a = {10,17,19,21,44,55,57,63,65,67};
+        int p = Partition.partitionHoare(a, 0, a.length - 1);
+
+        // pivot is 10 (first element), smallest already
+        assertEquals(0, p);
+        assertArrayEquals(new int[]{10,17,19,21,44,55,57,63,65,67}, a);
+    }
+
+    @Test
+    void hoare_emptyArray() {
+        int[] a = {};
+        int p = Partition.partitionHoare(a, 0, 0);
+
+        assertEquals(-1, p);
+        assertArrayEquals(new int[]{}, a);
+    }
+
+    @Test
+    void hoare_givenArray() {
+        int[] a = {84,3,7,1,9,6,2,5};
+        int p = Partition.partitionHoare(a, 0, a.length - 1);
+
+        // pivot is 84, so it ends up on the far right after partitioning
+        assertEquals(6, p);
+        assertArrayEquals(new int[]{5,3,7,1,9,6,2,84}, a);
+    }
+
 }
