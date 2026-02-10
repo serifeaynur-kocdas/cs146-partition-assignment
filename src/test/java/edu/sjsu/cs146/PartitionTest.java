75079b6334be70f308c3337868ea05fe1580c98f
Serife Aynur Kocdas <kocdasserifeaynur@gmail.com>
2026-02-09 17:17:43 -0800

    Add unit tests for Lomuto partition

---
+++ src/test/java/edu/sjsu/cs146/PartitionTest.java
@@ -0,0 +1,37 @@
+package edu.sjsu.cs146;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class PartitionTest {
+
+    @Test
+    void lomuto_sortedArray() {
+        int[] a = {10,17,19,21,44,55,57,63,65,67};
+        int p = Partition.partitionLomuto(a, 0, a.length - 1);
+
+        // pivot is 67, already the largest, so it stays at the end
+        assertEquals(9, p);
+        assertArrayEquals(new int[]{10,17,19,21,44,55,57,63,65,67}, a);
+    }
+
+    @Test
+    void lomuto_emptyArray() {
+        int[] a = {};
+        int p = Partition.partitionLomuto(a, 0, 0);
+
+        assertEquals(-1, p);
+        assertArrayEquals(new int[]{}, a);
+    }
+
+    @Test
+    void lomuto_givenArray() {
+        int[] a = {84,3,7,1,9,6,2,5};
+        int p = Partition.partitionLomuto(a, 0, a.length - 1);
+
+        // Since 5 is the pivot, the array should end up like this
+        assertEquals(3, p);
+        assertArrayEquals(new int[]{3,1,2,5,9,6,7,84}, a);
+    }
+}
